f00598de90c63357c09dbbf3bdecf85263b54dea
yonikim <yonikim@wisenut.co.kr>
2020-06-17 16:28:49 +0900

    link 파싱까지 구현

--- engine/doc_engine/courtauction_doc.js
+++ engine/doc_engine/courtauction_doc.js
@@ -1,9 +1,10 @@
+const querystring = require("querystring");
 const puppeteer = require("puppeteer");
 const moment = require("moment");
 const fs = require("fs-extra");
 const iconv = require("iconv-lite");
 
-const logger = require("logger");
+const logger = require("../../lib/logger");
 
 const getFiles = function(path, files) {
   fs.readdirSync(path).forEach(function(file) {
@@ -18,33 +19,20 @@ const getFiles = function(path, files) {
   });
 };
 
-//EUC-KR 인코딩.
-var strUtf8Query = decodeURIComponent(keyword);
-var buf = iconv.encode(strUtf8Query, "euc-kr");
-var encodeStr = "";
-for (var i = 0; i < buf.length; i++) {
-  encodeStr += "%" + buf[i].toString("16");
-}
-encodeStr = encodeStr.toUpperCase();
+// TD DO LIST
+// 1. field 정의 후 inject script 수정
+// 2. attach file 수집
 
 const engine = (function() {
   const execute = function(params, callback) {
-    // const requestUrl = params.requestUrl;
     const collectDataPath = params.collectDataPath;
     const userAgent = params.userAgent;
-
     const customer = params.customer;
     const source = params.source;
-
     const injectScripts = params.injectScripts;
     const docSelectors = params.rule.docSelectors;
-
-    const linkCollectorPath = collectDataPath + customer + "/";
-    let linkCollectors = [];
-    logger.info("### linkCollector Path :: " + linkCollectorPath);
-    getFiles(linkCollectorPath, linkCollectors);
-
-    var content = "";
+    const linkSelectors = params.rule.linkSelectors;
+    const crawlerInterval = linkSelectors.crawlerInterval;
 
     const now = moment();
 
@@ -76,23 +64,44 @@ const engine = (function() {
           height: 960
         });
 
+        logger.debug("[chrome] Step #1. link 리스트 배열에 담기");
+        const linkCollectorPath =
+          collectDataPath + "linkCollector/" + customer + "/";
+        logger.debug(" ### linkCollector Path :: " + linkCollectorPath);
+        let linkCollectors = [];
+        getFiles(linkCollectorPath, linkCollectors);
+
+        logger.debug("[chrome] Step #2. link url 파싱해서 배열에 담기");
         let links = [];
         for (linkCollector of linkCollectors) {
           let jsons = fs.readJSONSync(linkCollector);
           for (json of jsons) {
+            let param = querystring.parse(json.link);
+
+            // EUC-KR 인코딩
+            var strUtf8Query = decodeURIComponent(param.jiwonNm);
+            var buf = iconv.encode(strUtf8Query, "euc-kr");
+            var encodeStr = "";
+            for (var i = 0; i < buf.length; i++) {
+              encodeStr += "%" + buf[i].toString("16");
+            }
+            encodeStr = encodeStr.toUpperCase();
+
+            json.link = json.link.replace(param.jiwonNm, encodeStr);
+            // logger.info(json.link);
             links.push(json.link);
           }
         }
+        logger.debug(" ### " + links.length);
 
         try {
           // 수집된 링크 만큼 for문 돌기
           for (link of links) {
             // 수집 URL 접근
-            logger.debug("[chrome] Step #1. URL 접근");
+            logger.debug("[chrome] Step #3. URL 접근");
             logger.debug("[chrome] url : " + link);
             await page.goto(link);
 
-            // 인젝트 스크립트 설정 (주의! 인젝트 스크립트는 URL 접근 후 세팅 되어야함)
             for (let idx = 0; idx < injectScripts.length; idx++) {
               await page.addScriptTag({
                 path: injectScripts[idx]
@@ -103,7 +112,7 @@ const engine = (function() {
             }
 
             // 문서 추출
-            logger.debug("[chrome] Step #3. 문서 파싱");
+            logger.debug("[chrome] Step #4. 문서 파싱");
             var result = await page.evaluate(function(selectors) {
               return __parseDocument(selectors);
             }, docSelectors);
@@ -115,29 +124,11 @@ const engine = (function() {
               await browser.close();
               throw Error("DOCUMENT_PARSING_RULE_IS_NOT_MATCH");
             } else {
-              content = result.doc_content + "#" + keyword;
-              var doc_datetime = moment(new Date(result.doc_datetime)).format(
-                "YYYY-MM-DD-HHmmss"
-              );
-
-              result.doc_title = source;
-              result.doc_content = parser(content);
               result.doc_url = link;
-              result.img_url = [];
 
-              result.view_count = 0;
-              result.dislike_count = 0;
-              result.share_count = 0;
-              result.locations = "";
               result.source = source;
               result.search_keyword_text = keyword;
               result.customer_id = customer;
-              // result.request_seq = requestSeq;
-              result.pub_year = doc_datetime.split("-")[0];
-              result.pub_month = doc_datetime.split("-")[1];
-              result.pub_day = doc_datetime.split("-")[2];
-              result.pub_time = doc_datetime.split("-")[3];
-              result.doc_datetime = undefined;
 
               result.uuid = md5Generator(
                 link,
@@ -146,47 +137,26 @@ const engine = (function() {
                 result.pub_day
               );
 
-              result.depth1_seq = 3;
-              result.depth2_seq = 17;
-              result.depth3_seq = 0;
-              result.depth1_nm = "SNS";
-              result.depth2_nm = "인스타그램";
-              result.depth3_nm = null;
-              result.doc_second_url = "NULL";
-
-              result.comments = [];
-
-              logger.debug("[chrome] Step #4. 댓글 파싱 ");
-              let comments = await page.evaluate(function(selector) {
-                return __parseComment(selector);
-              }, commentSelectors);
-
-              // logger.info(comments);
-
-              if (comments === null) {
-                await browser.close();
-                throw Error("COMMENT_PARSING_RULE_IS_NOT_MATCH");
-              } else {
-                comments.forEach(function(comment) {
-                  comment.cmt_datetime = moment(
-                    new Date(comment.cmt_datetime)
-                  ).format("YYYY-MM-DD-HHmmss");
-                });
-                result.comments = result.comments.concat(comments);
-              }
-              result.comment_count = comments.length;
-
               // 저장
               const fileName =
                 "D-1-" + now.format("YYYYMMDDHHmm-ssSSS") + ".json";
+              logger.debug("[chrome] Step #5. 저장 ");
+              let folderPath =
+                collectDataPath +
+                "docCollector/" +
+                customer +
+                "/" +
+                now.format("YYYYMMDD");
+
+              if (!fs.existsSync(folderPath)) {
+                await fs.mkdirSync(folderPath);
+              }
+
               logger.debug(
-                "[chrome] Step #5. 저장 " +
-                  collectDataPath +
-                  path.sep +
-                  fileName
+                "[chrome] ### filePath: " + folderPath + "/" + fileName
               );
               await fs.writeFile(
-                collectDataPath + path.sep + fileName,
+                folderPath + "/" + fileName,
                 JSON.stringify(result),
                 err => {
                   if (err) throw err;
@@ -230,28 +200,36 @@ engine.execute(
   {
     requestUrl:
       "https://www.courtauction.go.kr/RetrieveRealEstMulDetailList.laf?",
+    startDt: "2020-06-10",
+    endDt: "2020-06-16",
     customer: "courtauction",
     collectDataPath:
-      "/Users/yoni/d-platform/30.DMap/00.workspace/auction/collect_data/linkCollector/",
+      "/Users/yoni/d-platform/30.DMap/00.workspace/auction/collect_data/",
     userAgent:
       "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)",
-    requestSeq: 0,
-    source: "법원경매정보",
+    source: "대한민국 법원경매정보",
     injectScripts: [
+      "../inject_script/listParser.js",
       "../inject_script/documentParser.js",
-      "../inject_script/datetimeParser.js",
-      "../inject_script/commentParser.js"
+      "../inject_script/datetimeParser.js"
     ],
     rule: {
+      linkSelectors: {
+        crawlerInterval: 3000,
+        listSelector: "tr[class^=Ltbl_list_lvl] td.txtleft div[class^=tbl_btm]",
+        linkSelector: "a:first-child",
+        linkAttr: "javascript:onclick",
+        linkPatternRegex: "\\(\\'(.*)\\'\\,.*\\'(.*)\\'\\,",
+        linkPattern:
+          "https://www.courtauction.go.kr/RetrieveRealEstCarHvyMachineMulDetailInfo.laf?saNo=#2#&jiwonNm=#1#"
+      },
       docSelectors: {
         documentSelector: "main[class] article[class]",
         documentNoResultSelector: "div.error-container",
         contentSelector: "div:nth-child(3) > div > ul > div[role] span",
         writerSelector: "div:nth-child(2) > div[class] > div:first-child a",
         datetimeSelector: "time[datetime]",
-        datetimeAttr: "datetime",
-        likeCountSelector: "div section:nth-child(2) button span",
-        InstaCommentCountSelector: "div > div > ul li > div > div"
+        datetimeAttr: "datetime"
       }
     }
   },
